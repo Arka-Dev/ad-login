83ea6ad8c54860093d7b885d3d30d61c6d3974b8
brojen <brojendra.das@mjunction.in>
2024-01-12 10:49:15 +0530

    feat(others): implementation of user details after login

--- src/common/libraries/active-directory/mj-active-directory.library.ts
+++ src/common/libraries/active-directory/mj-active-directory.library.ts
@@ -5,7 +5,7 @@ import * as ldap from 'ldapjs';
 @Injectable()
 export class MjActiveDirectory {
 
-    private _ldapHost = '10.130.5.44';
+    private _ldapHost = '172.16.2.5';
     private _ldapPort = 389;
     private _domainEmail = 'metaljunction.com';
 
@@ -26,8 +26,12 @@ export class MjActiveDirectory {
         }
         try{
             status = await this.bind(username, password);
-            //user = await this.getUser(username);
-            //console.log('+++++', user);
+            try {
+                user = await this.getUser(username);
+                console.log('+++++', user);
+            } catch(err) {
+                console.log('Error', err);
+            }
         }
         catch(err){
             message = err;
@@ -51,9 +55,28 @@ export class MjActiveDirectory {
     }
 
     async getUser(username: string): Promise<any> {
-        return this.client.search(username, {
-            scope: 'base',
-            attributes: ['uid', 'dn', 'cn', 'mail']
+        const options = {
+            scope: "sub",
+            filter: `(email=brojendra.das@mjunction.in)`
+        };
+        return this.client.search('dc=metaljunction,dc=com', options, (err, res) => {
+            if (err) {
+                console.log('Failed to search', err);
+            } 
+            
+            res.on('searchEntry', entry => {
+                console.log(entry.object);
+            });
+            res.on('searchReference', referral => {
+                console.log('referral: ' + referral.uris.join());
+            });
+            res.on('error', err => {
+                console.error('error: ' + err.message);
+            });
+            res.on('end', result => {
+                console.log(result);
+            });
+            
         });
     }
 
